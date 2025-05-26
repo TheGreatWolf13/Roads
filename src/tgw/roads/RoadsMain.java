8e9aeef42a8aa786eb607cbaa04035cb54b10e4c
TheGreatWolf <16447647+thegreatwolf13@users.noreply.github.com>
2025-05-26 13:36:50 -0300

    Implemented basic window.

--- src/tgw/roads/RoadsMain.java
+++ src/tgw/roads/RoadsMain.java
@@ -1,10 +1,79 @@
 package tgw.roads;
 
+import org.lwjgl.glfw.Callbacks;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.system.MemoryStack;
+import org.lwjgl.system.MemoryUtil;
+
+import java.nio.IntBuffer;
+
 public final class RoadsMain {
 
     private RoadsMain() {}
 
+    private static long init() {
+        GLFWErrorCallback.createPrint(System.err).set();
+        if (!GLFW.glfwInit()) {
+            throw new IllegalStateException("Unable to initialize GLFW!");
+        }
+        GLFW.glfwDefaultWindowHints();
+        GLFW.glfwWindowHint(GLFW.GLFW_VISIBLE, GLFW.GLFW_FALSE);
+        GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GLFW.GLFW_TRUE);
+        long window = GLFW.glfwCreateWindow(300, 300, "Hello World!", MemoryUtil.NULL, MemoryUtil.NULL);
+        if (window == MemoryUtil.NULL) {
+            throw new IllegalStateException("Failed to create GLFW window!");
+        }
+        GLFW.glfwSetKeyCallback(window, (w, key, scancode, action, mods) -> {
+            if (key == GLFW.GLFW_KEY_ESCAPE && action == GLFW.GLFW_RELEASE) {
+                GLFW.glfwSetWindowShouldClose(w, true);
+            }
+        });
+        try (MemoryStack stack = MemoryStack.stackPush()) {
+            IntBuffer pWidth = stack.mallocInt(1);
+            IntBuffer pHeight = stack.mallocInt(1);
+            GLFW.glfwGetWindowSize(window, pWidth, pHeight);
+            GLFWVidMode vidmode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor());
+            assert vidmode != null;
+            GLFW.glfwSetWindowPos(window, (vidmode.width() - pWidth.get(0)) / 2, (vidmode.height() - pHeight.get(0)) / 2);
+        }
+        GLFW.glfwMakeContextCurrent(window);
+        GLFW.glfwSwapInterval(1);
+        GLFW.glfwShowWindow(window);
+        return window;
+    }
+
+    private static void loop(long window) {
+        GL.createCapabilities();
+        GL11.glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+        int frames = 0;
+        String fps = "";
+        double lastTime = GLFW.glfwGetTime();
+        while (!GLFW.glfwWindowShouldClose(window)) {
+            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+            ++frames;
+            double time = GLFW.glfwGetTime();
+            if (time - lastTime >= 1) {
+                lastTime = time;
+                fps = frames + " FPS";
+                frames = 0;
+            }
+            GLFW.glfwSetWindowTitle(window, fps);
+            GLFW.glfwSwapBuffers(window);
+            GLFW.glfwPollEvents();
+        }
+        Callbacks.glfwFreeCallbacks(window);
+        GLFW.glfwDestroyWindow(window);
+        GLFW.glfwTerminate();
+        //noinspection DataFlowIssue
+        GLFW.glfwSetErrorCallback(null).free();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        long window = init();
+        loop(window);
     }
 }
